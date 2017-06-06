2b7b37c9bba1e6f32efe742a44f1703d05dbc88c
sujitharajendran <sujikavi66@gmail.com>
2017-06-06 14:26:22 +0530

    Add files via upload

---
+++ check.java
@@ -0,0 +1,17 @@
+package check;
+
+public class check {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int number=1;
+ if(number>0)
+ {
+	 System.out.println("the number is positive:"+number);
+ }
+ else
+	 {
+	 System.out.println("the number is negative:"+number);
+	 }
+	}
+}
